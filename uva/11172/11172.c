3539aecaef17d7d79d60d9261fd94823f2ae1f27
wesleymesquita <wesleymesquita@gmail.com>
2017-04-25 09:08:52 -0300

    11172

---
+++ uva/11172/11172.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main(){
+    
+   int t, a, b;
+   scanf("%d", &t);
+
+   for(;t-->0;){
+       scanf("%d %d", &a, &b);
+       if(a<b)
+           printf("<\n");
+       else if(a>b)
+           printf(">\n");
+       else
+           printf("=\n");
+
+   }
+    
+   return 0;
+}
