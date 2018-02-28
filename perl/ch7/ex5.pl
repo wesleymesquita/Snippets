09c88e4aa27ed3791f4ad69f6b6537096b93e255
wesleymesquita <wesleymesquita@gmail.com>
2018-02-28 18:45:46 -0300

    ch7

---
+++ perl/ch7/ex5.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+use 5.010;
+
+if($#ARGV != 0){
+	die "Use ./ex1.pl [FileName]";
+}
+open my $file, '<', $ARGV[0]
+	or die "invalid file name $ARGV[1]";
+
+while( <$file> ){
+	if(/((.)\2).*\1/){
+		print $_;
+	}
+}
+
+
