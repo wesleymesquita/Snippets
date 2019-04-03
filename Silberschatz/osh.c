2b2bc9d8b4d124d08d81bbb4b30c2ea051688e31
Wesley Mesquita <wesleymesquita@gmail.com>
2019-04-03 14:42:14 -0600

    Phases 1 and 2

--- Silberschatz/osh.c
+++ Silberschatz/osh.c
@@ -1,15 +1,17 @@
-/* Silberschatz 9th edition - Chapter 3 - Project 1 */
+/* Silberschatz 9th edition - Chapter 3 - Project 1 (Phases 1 and 2) 
+*/
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h> 
 #include <errno.h>
 #include <sys/wait.h>
+#include <ctype.h>
 
 #define MAX_LINE 80
+#define MAX_HISTORY 10
 
-int 
-parse_cmd_line(char *args[MAX_LINE/2 + 1], const char *line)
+int parse_cmd_line(char *args[MAX_LINE/2 + 1], const char *line)
 {
 	ssize_t line_len, i, k, nargs;
 		
@@ -31,8 +33,7 @@ parse_cmd_line(char *args[MAX_LINE/2 + 1], const char *line)
 }
 
 
-void
-free_cmd_args(char *args[], int nargs)
+void free_cmd_args(char *args[], int nargs)
 {
 	int i;
 	for(i=0; i<nargs; i++)
@@ -41,8 +42,7 @@ free_cmd_args(char *args[], int nargs)
 } 
 
 
-int 
-test__parse_cmd_line()
+int test__parse_cmd_line()
 {
 	const char *line = "ls -altrh &";
 	char *args[MAX_LINE/2 + 1];
@@ -67,16 +67,99 @@ test__parse_cmd_line()
 	return 0;
 }	
 
-int
-main()
+struct history{
+	char historic[MAX_LINE/2 + 1][10];
+	int nentries;
+	int top;
+};
+
+void history_init(struct history *history)
+{
+	int i;
+
+	for(i=0; i<MAX_HISTORY; i++)
+		history->historic[i][0] = '\0';
+	history->top = MAX_HISTORY;
+	history->nentries = 0;
+}
+
+void history_add_entry(struct history *history, const char* entry)
+{
+	if(strcmp(history->historic[history->top], entry) == 0)
+		return;	
+	
+	if(history->top == 0)
+		history->top = MAX_HISTORY;
+	
+	history->top--;
+	
+	if(history->nentries < MAX_HISTORY)
+		history->nentries++;	
+	strcpy(history->historic[history->top], entry);
+}
+
+void history_print(struct history *history)
+{		
+	int entries, top;
+	entries = history->nentries;
+	top = history->top-1;
+	while(entries > 0){
+		if(top < 0 || history->historic[top][0]=='\0')
+			top = MAX_HISTORY-1;
+		printf("%d %s\n", entries, history->historic[top]);	
+		entries--; top--;
+	}		  
+}
+
+int history_get_cmd(struct history *history, char* cmd, char *args[MAX_LINE/2 + 1])
+{
+	char cmd_arg[16];	
+	int i, cmd_len, ncmd;
+	
+	/* there is no command to repeat */
+	if(history->nentries == 0){
+		fprintf(stderr, "event not found\n");
+		return 1;
+	}
+	if(cmd[1] == '!') {
+			parse_cmd_line(args, history->historic[history->top]);
+			return 0;
+	}
+	cmd_len = strlen(cmd);
+	
+	/*	check if all chars after `!' are numeric
+		if not return error */		
+	for(i=1; i<cmd_len; i++){
+		if(!isdigit(cmd[i])){
+			fprintf(stderr, "event not found\n");
+			return 1;
+		}
+		cmd_arg[i-1]=cmd[i];
+	}
+	cmd_arg[i]='\0';				
+	ncmd = atoi(cmd_arg);
+	i = history->top;
+	while(ncmd > 1){
+		i = i == MAX_HISTORY-1 ? 0 : i+1;
+		ncmd--;	
+	}	
+	parse_cmd_line(args, history->historic[i]);
+	return 0;
+}
+
+int main()
 {
 	char *args[MAX_LINE/2 + 1], line[MAX_LINE];
+	struct history history;
+	
 	int should_run, nargs;
 	pid_t pid, ret_wait;	
 	int wstatus, must_wait;
 	
 	if(test__parse_cmd_line() != 0)
 		return EXIT_FAILURE;
+	
+	history_init(&history);
 
 	should_run = 1;
 	while(should_run){
@@ -84,6 +167,8 @@ main()
 		printf("osh>");
 		fflush(stdout);	
 		fgets(line, MAX_LINE, stdin);
+		/* Just strip out the new line character */
+		line[strlen(line)-1] = '\0';
 		nargs = parse_cmd_line(args, line);
 		if(nargs == 0){
 			fprintf(stderr, "osh errror : No input command\n");
@@ -93,6 +178,19 @@ main()
 		if(strcmp(args[0], "exit") == 0){
 			should_run = 0;
 			free_cmd_args(args, nargs);		
+			continue;
+		}
+		
+
+		if(args[0][0] ==  '!'){
+			if(history_get_cmd(&history, args[0], args) != 0)
+				continue;
+		}else{
+				history_add_entry(&history, line);
+		}	
+	
+		if(strcmp(args[0], "history") == 0){
+			history_print(&history);
 			continue;
 		}
 		
@@ -118,6 +216,5 @@ main()
 			free_cmd_args(args, nargs);	
 		}			
 	}
-
 	return 0;
 }
