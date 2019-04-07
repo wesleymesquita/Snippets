84d71ec2ecd3dd7dbea2483c26405f927869bdfd
Wesley Mesquita <wesleymesquita@gmail.com>
2019-04-07 08:20:03 -0600

    Remove hidden files added by mistake

--- Silberschatz/task_it/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/oscreader/osc9e-src/ch3/task_it/built-in.o :=  rm -f /home/oscreader/osc9e-src/ch3/task_it/built-in.o; ar rcsD /home/oscreader/osc9e-src/ch3/task_it/built-in.o
--- Silberschatz/task_it/.tmp_versions/task_iterator.mod
+++
@@ -1,2 +0,0 @@
-/home/oscreader/osc9e-src/ch3/task_it/task_iterator.ko
-/home/oscreader/osc9e-src/ch3/task_it/task_iterator.o
