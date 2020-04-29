0da008834c3bfc7d2cb317258c92a2b31026838d
wesleymesquita <wesleymesquita@gmail.com>
2020-04-29 08:55:02 -0300

    Exercise 15 - Intr to Lin Alg - Strang 4ed

---
+++ linalg/Strang_Intro_to_Lin_Alb_ex_15.m
@@ -0,0 +1,36 @@
+% Plotting for Introduction to Linear Algebra, 4th edition
+
+%Exercise 15 :
+% By the proportions showed in figure 1.5 a, lets assume w = [1 4]'
+% and v = [4 1]'
+
+w = [1 4]';
+v = [4 1]';
+
+hold on
+%plot w
+quiver(0,0, w(1), w(2), 0);
+
+%plot v
+quiver(0,0,v(1), v(2), 0);
+
+%plot the dashed line (w-v) from v
+line([v(1) w(1)], [v(2), w(2)], 'LineStyle', '--');
+
+%plot point u 
+% now we can have point u = 0.5v + 0.5w
+u = 0.5*v + 0.5*w;
+plot(u(1), u(2), "s");
+
+u = 3/4*v + 1/4*w;
+plot(u(1), u(2), "s");
+
+u = 1/4*v + 1/4*w;
+plot(u(1), u(2), "s");
+
+u = v + w;
+quiver(0,0, u(1), u(2));
+
+legend("w", "v", "w-v", "u = 0.5*v + 0.5*w", "3/4*v + 1/4*w", "1/4*v + 1/4*w", "v+w"); 
+
+hold off
